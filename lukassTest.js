9c2499e1fc84f7807acf4e56cc1c7dcd946bf794
timorss <timorss@gmail.com>
2021-11-03 23:54:45 +0200

    nft marke,t listing, sell, my nfts

---
+++ lukassTest.js
@@ -0,0 +1,14 @@
+
+const man = {
+    name: 'lukass',
+    age: 30,
+    hobies: ['nft', 'music'],
+}
+console.log('********')
+console.log(man)
+console.log(typeof man)
+
+
+console.log('********')
+console.log(JSON.stringify(man));
+console.log(typeof JSON.stringify(man));
